7faa8c3e1720f9c6c14d1628f59def7e2cfb21cd
JingYi Li <li000848@algonquinlive.com>
2024-01-19 16:57:59 -0500

    Commit 4 Added extra lines

--- app/src/main/java/algonquin/cst2335/li000848/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/li000848/MainActivity.java
@@ -10,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
